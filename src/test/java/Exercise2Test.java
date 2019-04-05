b2de1ae3e41bfd50882ebbeb026662308ff7940b
voontv <voontv@gmail.com>
2019-04-05 08:40:11 +0700

    Exercise 2

---
+++ src/test/java/Exercise2Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Exercise2Test {
+
+    @Test
+    public void test_totalDigits() {
+        Exercise2 exercise2 = new Exercise2();
+        assertEquals(exercise2.totalDigits(27),9);
+    }
+
+    @Test
+    public void test_multiplicationDigits() {
+        Exercise2 exercise2 = new Exercise2();
+        assertEquals(exercise2.multiplicationDigits(27),14);
+    }
+}
