f8ad2addac708e28c9457b83302b6876cf57c157
voontv <voontv@gmail.com>
2019-04-05 08:16:16 +0700

    completed exercise 1

---
+++ src/main/java/Exercise1.java
@@ -0,0 +1,21 @@
+public class Exercise1 {
+
+    public int getLargestCommonDivisor(int integerA, int integerB) {
+        if(integerA == 0 || integerB ==0) {
+            return Math.abs((int) (integerA+integerB));
+        }
+
+        while(integerA != integerB) {
+            if(integerA >= integerB) {
+                integerA -= integerB;
+            } else {
+                integerB -= integerA;
+            }
+        }
+        return integerA;
+    }
+
+    public int getLowestCommonMultiples(int integerA, int integerB) {
+        return (integerA * integerB) / getLargestCommonDivisor(integerA, integerB);
+    }
+}
