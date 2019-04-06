af92b919c13679f9a5b6a35a9df4af0a02caabf4
voontv <voontv@gmail.com>
2019-04-06 21:49:50 +0700

    Exercise14

---
+++ src/main/java/Exercise14.java
@@ -0,0 +1,26 @@
+public class Exercise14 {
+
+    public double getFactorial(int factorial) {
+        double result = 1.0;
+        if(factorial == 0) {
+            return 1.0;
+        }
+
+        for(int integer = 1; integer <= factorial; integer++) {
+            result *= integer;
+        }
+        return result;
+    }
+
+    public double totalFactorialInRange(double epsilon) {
+        double result = 0.0;
+        int integer = 0;
+        double eps;
+        do {
+            eps = 1.0 / getFactorial(integer);
+            result += eps;
+            integer++;
+        } while (Double.compare(Math.sqrt(eps),epsilon)>0);
+        return result;
+    }
+}
