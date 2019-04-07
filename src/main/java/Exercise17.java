0cd1c1e699650fee4b61dcd0ad9b28042695c048
voontv <voontv@gmail.com>
2019-04-07 20:45:28 +0700

    Exercise 17

---
+++ src/main/java/Exercise17.java
@@ -0,0 +1,44 @@
+public class Exercise17 {
+
+    public int countNumberDigit(long number) {
+        int count = 0;
+        while (number > 0) {
+            count++;
+            number /= 10;
+        }
+        return count;
+    }
+
+    public boolean isLengthDigitCorrectRule(long number) {
+        return (countNumberDigit(number) >= 2 && countNumberDigit(number) <= 5);
+    }
+
+    public boolean isDigitCorrectRule(long number) {
+        long result = 0;
+        long tempNumber = number;
+        int lengthDigit = countNumberDigit(number);
+
+        if(!isLengthDigitCorrectRule(tempNumber)) {
+            return false;
+        }
+
+        do {
+            result += Math.pow(tempNumber % 10,lengthDigit);
+            tempNumber /= 10;
+        } while(tempNumber>0);
+        return (result == number);
+    }
+
+    public StringBuilder printisDigitCorrectRuleInRange() {
+        int number = 10;
+        StringBuilder builder = new StringBuilder();
+
+        while(isLengthDigitCorrectRule(number)) {
+            if(isDigitCorrectRule(number)) {
+                builder.append(number).append(" ");
+            }
+            number++;
+        }
+        return  builder;
+    }
+}
